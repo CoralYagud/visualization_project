bc50f46ec96b0cc0fd0be4a11702b57779857be4
Coral Yagud <162157135+CoralYagud@users.noreply.github.com>
2024-07-12 17:24:08 +0300

    Update visualization_project.py

--- visualization_project.py
+++ visualization_project.py
@@ -28,6 +28,10 @@ def load_data():
 
 food_data_melted, trade_data, need_data = load_data()
 
+# Define colorblind-friendly colors
+exporting_color = '#1f77b4'  # Blueish-green
+non_exporting_color = '#d62728'  # Dark red
+
 # Initialize Dash app
 app = dash.Dash(__name__)
 server = app.server  # Add this line
@@ -42,32 +46,26 @@ app.layout = html.Div([
                         'fontSize': '40px',
                         'fontWeight': 'bold',
                         'margin': '10px 0',
-                        'textShadow': '2px 2px 4px black'  # Adding shadow effect for black border
+                        'textShadow': '2px 2px 4px black'
                     }),
             html.H3("Analyzing Israel's Vulnerability to Food Shortages Controlled by Global Market Leaders",
                     style={
                         'color': 'white',
                         'fontSize': '24px',
                         'margin': '0',
-                        'textShadow': '2px 2px 4px black'  # Adding shadow effect for black border
+                        'textShadow': '2px 2px 4px black'
                     }),
         ], style={
-            'backgroundImage': 'url(/assets/War_on_the_Plate.jpg)',
+            'backgroundImage': 'url(/assets/War_on_the_Plate.png)',
             'backgroundSize': 'cover',
             'textAlign': 'center',
             'padding': '40px 0',
-            'border': '5px solid silver',  # Changing border to black
+            'border': '5px solid silver',
             'borderRadius': '10px'
         }),
     ], style={'display': 'flex', 'flexDirection': 'column', 'alignItems': 'center', 'justifyContent': 'center', 'marginBottom': '20px'}),
+
     html.Div([
-        html.Label("Select Country:", style={'fontSize': '16px', 'fontWeight': 'bold'}),
-        dcc.Dropdown(
-            id='country-dropdown',
-            options=[{'label': country, 'value': country} for country in food_data_melted['Entity'].unique()],
-            value='Israel',
-            style={'marginBottom': '10px'}
-        ),
         html.Label("Select Food Item:", style={'fontSize': '16px', 'fontWeight': 'bold'}),
         dcc.Dropdown(
             id='food-dropdown',
@@ -75,6 +73,7 @@ app.layout = html.Div([
             style={'marginBottom': '20px'}
         )
     ], style={'width': '50%', 'margin': '0 auto', 'padding': '20px', 'boxShadow': '0px 0px 15px rgba(0, 0, 0, 0.1)', 'borderRadius': '10px', 'backgroundColor': '#f9f9f9'}),
+
     html.Div([
         dcc.Graph(id='global-map', style={'marginBottom': '20px'}),
         dcc.Slider(
@@ -96,19 +95,40 @@ app.layout = html.Div([
             n_intervals=0,
             disabled=True  # initially disabled
         ),
-        dcc.Graph(id='country-line-chart')
-    ], style={'textAlign': 'center', 'margin': '0 auto', 'width': '80%'}),
+    ], style={'textAlign': 'center', 'marginBottom': '20px'}),
+    html.Div([
+        html.Label("Select Country:", style={'fontSize': '16px', 'fontWeight': 'bold'}),
+        dcc.Dropdown(
+            id='country-dropdown',
+            options=[{'label': country, 'value': country} for country in food_data_melted['Entity'].unique()],
+            value='Israel',
+            style={'marginBottom': '20px'}
+        ),
+        html.Label("Select Number of Top Countries:", style={'fontSize': '16px', 'fontWeight': 'bold'}),
+        dcc.Input(id='num-countries', type='number', value=20, min=1, style={'marginBottom': '20px'}),
+    ], style={'width': '50%', 'margin': '0 auto', 'padding': '20px', 'boxShadow': '0px 0px 15px rgba(0, 0, 0, 0.1)', 'borderRadius': '10px', 'backgroundColor': '#f9f9f9'}),
+
+    html.Div([
+        html.Div([
+            dcc.Graph(id='heatmap', style={'height': '800px'})
+        ], style={'width': '50%', 'display': 'inline-block', 'verticalAlign': 'top'}),
+        html.Div([
+            dcc.Graph(id='country-line-chart')
+        ], style={'width': '50%', 'display': 'inline-block', 'verticalAlign': 'top'}),
+    ]),
 ], style={'fontFamily': 'Arial, sans-serif', 'backgroundColor': '#f4f4f4', 'padding': '20px'})
 
 # Callbacks
 @app.callback(
     [Output('global-map', 'figure'),
-     Output('country-line-chart', 'figure')],
+     Output('country-line-chart', 'figure'),
+     Output('heatmap', 'figure')],
     [Input('country-dropdown', 'value'),
      Input('food-dropdown', 'value'),
-     Input('year-slider', 'value')]
+     Input('year-slider', 'value'),
+     Input('num-countries', 'value')]
 )
-def update_charts(selected_country, selected_food, selected_year):
+def update_charts(selected_country, selected_food, selected_year, num_countries):
     filtered_food_data = food_data_melted[(food_data_melted['Year'] == selected_year) & (food_data_melted['Food Item'] == selected_food)]
     country_food_data = food_data_melted[(food_data_melted['Entity'] == selected_country) & (food_data_melted['Food Item'] == selected_food)]
     filtered_trade_data = trade_data[(trade_data['Year'] == selected_year)]
@@ -121,10 +141,10 @@ def update_charts(selected_country, selected_food, selected_year):
             locations=merged_data['Entity'],
             locationmode='country names',
             z=merged_data['Production (tonnes)'],
-            colorscale='Viridis',
+            colorscale='Cividis',
             marker_line_width=1,
-            marker_opacity=0.8,
-            marker_line_color=merged_data['Export Value'].apply(lambda x: 'green' if pd.notnull(x) else 'red'),
+            marker_opacity=1,
+            marker_line_color=merged_data['Export Value'].apply(lambda x: exporting_color if pd.notnull(x) else non_exporting_color),
             showscale=True,
             visible=True
         ),
@@ -132,9 +152,9 @@ def update_charts(selected_country, selected_food, selected_year):
             locations=merged_data['Entity'],
             locationmode='country names',
             z=merged_data['Production (tonnes)'],
-            colorscale='Viridis',
+            colorscale='Cividis',
             marker_line_width=1,
-            marker_opacity=0.8,
+            marker_opacity=1,
             showscale=True,
             visible=False
         ),
@@ -145,7 +165,7 @@ def update_charts(selected_country, selected_food, selected_year):
             colorscale=[[0, 'rgba(0,0,0,0)'], [1, 'rgba(0,0,0,0)']],
             marker_line_width=1,
             marker_opacity=0.8,
-            marker_line_color=merged_data['Export Value'].apply(lambda x: 'green' if pd.notnull(x) else 'red'),
+            marker_line_color=merged_data['Export Value'].apply(lambda x: exporting_color if pd.notnull(x) else non_exporting_color),
             showscale=False,
             visible=False
         )
@@ -176,25 +196,25 @@ def update_charts(selected_country, selected_food, selected_year):
             ),
         ],
         title={
-        'text': f'Global Production and Exports to Israel of {selected_food} in {selected_year}',
-        'font': {
-            'family': 'Arial, sans-serif',
-            'size': 24,
-            'color': 'black'
+            'text': f'Global Production and Exports to Israel of {selected_food} in {selected_year}',
+            'font': {
+                'family': 'Arial, sans-serif',
+                'size': 24,
+                'color': 'black'
+            },
+            'x': 0.5,
+            'xanchor': 'center'
         },
-        'x': 0.5,
-        'xanchor': 'center'
-    },
         legend=dict(x=0.05, y=0, traceorder='normal', font=dict(size=12)),
         height=600,
-        width=1200,
+        width=1300,
         annotations=[
             dict(
                 x=0.05, y=0.01,
                 xref='paper', yref='paper',
-                text='<span style="color:green">⬤</span> Exporting to Israel<br>'
-                     '<span style="color:red">⬤</span> Not Exporting to Israel<br>'
-                     '<span style="color:blue">▯</span> Production (tonnes)',
+                text=f'<span style="color:{exporting_color}">⬤</span> Exporting to Israel<br>'
+                    f'<span style="color:{non_exporting_color}">⬤</span> Not Exporting to Israel<br>'
+                    '<span style="color:blue">▯</span> Production (tonnes)',
                 showarrow=False,
                 align='left',
                 bgcolor='rgba(255, 255, 255, 0.8)',
@@ -229,16 +249,16 @@ def update_charts(selected_country, selected_food, selected_year):
         ))
 
     fig_line.update_layout(
-            title={
-        'text': f'Production of {selected_food} in {selected_country} Over Time',
-        'font': {
-            'family': 'Arial, sans-serif',
-            'size': 24,
-            'color': 'black'
+        title={
+            'text': f'Production of {selected_food} in {selected_country} Over Time',
+            'font': {
+                'family': 'Arial, sans-serif',
+                'size': 24,
+                'color': 'black'
+            },
+            'x': 0.5,
+            'xanchor': 'center'
         },
-        'x': 0.5,
-        'xanchor': 'center'
-    },
         xaxis=dict(
             rangeslider=dict(visible=True),
             type="date"
@@ -247,10 +267,41 @@ def update_charts(selected_country, selected_food, selected_year):
         hovermode='x unified',
         height=600,
         width=1200,
-        paper_bgcolor='rgba(0,0,0,0)'  # Making the paper background transparent
+        paper_bgcolor='rgba(0,0,0,0)'
     )
 
-    return fig_map, fig_line
+    # Update heatmap
+    filtered_data = food_data_melted[food_data_melted['Food Item'] == selected_food]
+    top_countries = filtered_data.groupby('Entity')['Production (tonnes)'].sum().nlargest(num_countries).index
+    filtered_data = filtered_data[filtered_data['Entity'].isin(top_countries)]
+
+    heatmap_data = filtered_data.pivot_table(index='Entity', columns='Year', values='Production (tonnes)', fill_value=0)
+    heatmap_data = heatmap_data.loc[top_countries]  # Ensure top countries are in correct order
+
+    fig_heatmap = px.imshow(heatmap_data,
+                            labels=dict(x="Year", y="Country", color="Production (tonnes)"),
+                            x=heatmap_data.columns, y=heatmap_data.index,
+                            aspect="auto", color_continuous_scale='Blues')
+
+    fig_heatmap.update_layout(
+        title={
+            'text': f'Production of {selected_food} Over Years',
+            'font': {
+                'family': 'Arial, sans-serif',
+                'size': 24,
+                'color': 'black'
+            },
+            'x': 0.5,
+            'xanchor': 'center'
+        },
+        xaxis=dict(title="Year", side="top"),
+        yaxis_title="Country",
+        coloraxis_colorbar=dict(title="Production (tonnes)"),
+        height=600,
+        paper_bgcolor='rgba(0,0,0,0)'
+    )
+
+    return fig_map, fig_line, fig_heatmap
 
 @app.callback(
     [Output('year-slider', 'value'),
@@ -285,5 +336,5 @@ def update_slider(n_intervals, play_clicks, stop_clicks, current_year, min_year,
 
 # Run app
 if __name__ == '__main__':
-    print("Starting the app...")
     app.run_server(debug=True)
+
